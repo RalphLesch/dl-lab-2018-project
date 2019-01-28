f180097bd08a2bbea5c0c2f74b37acb33e0aa2a0
Ralph Lesch <ralph.lesch@neptun.uni-freiburg.de>
2019-01-28 16:36:58 +0100

    run_train.sh modified for propper logging

--- code/run_train.sh
+++ code/run_train.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 #python Train_Net.py --checkpoint_dir=./checkpoints/ultraslimS_$1/ --configuration=$1
-python Train_Net.py --checkpoint_dir=./checkpoints/$1/ --dataset_dir=./data/$1/ --max_steps=2000
+#python Train_Net.py --dataset_dir=./data/data_CamVidV300/ --checkpoint_dir=./checkpoints/$2/ --max_steps=$3 --augmentation=$4
+cp=$1
+shift
+stdbuf -o0 -e0 python3 Train_Net.py --dataset_dir=./data/data_CamVidV300/ --checkpoint_dir=./checkpoints/$cp/ $@ 2>&1 | tee logs/$cp.log

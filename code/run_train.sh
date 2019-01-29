6cbdcb94d3b0c7bd1af71601fbc2e6d217a844b6
Ralph Lesch <ralph.lesch@neptun.uni-freiburg.de>
2019-01-29 21:55:51 +0100

    run_train.sh script fix

--- code/run_train.sh
+++ code/run_train.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# screen -S t1
+# source ../../venv3/bin/activate
+# python3 run_train.sh <subfolder: checkpoint and log> --aug_type=<type>
+
 #python Train_Net.py --checkpoint_dir=./checkpoints/ultraslimS_$1/ --configuration=$1
 #python Train_Net.py --dataset_dir=./data/data_CamVidV300/ --checkpoint_dir=./checkpoints/$2/ --max_steps=$3 --augmentation=$4
 cp=$1
 shift
-stdbuf -o0 -e0 python3 Train_Net.py --dataset_dir=./data/data_CamVidV300/ --checkpoint_dir=./checkpoints/$cp/ --max_steps=21000  $@ 2>&1 | tee logs/$cp.log
+
+mkdir logs/$cp
+stdbuf -o0 -e0 python3 Train_Net.py --dataset_dir=./data/data_CamVidV300/ --checkpoint_dir=./checkpoints/$cp/ --logs_path=./logs/$cp/ --max_steps=21000 $@ 2>&1 | tee logs/$cp/train.log

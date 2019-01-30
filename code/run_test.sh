031c096a18813006bef0b6419e4754b30db7df7d
Ralph Lesch <ralph.lesch@neptun.uni-freiburg.de>
2019-01-30 23:42:20 +0100

    run_test: append to log

--- code/run_test.sh
+++ code/run_test.sh
@@ -2,4 +2,4 @@
 
 cp=$1
 shift
-python Test_Net.py --model_path=./checkpoints/$cp --dataset_dir=./data/data_CamVidV300/ --logs_path=./logs/$cp/ $@ 2>&1 | tee logs/$cp/test.log
+python Test_Net.py --model_path=./checkpoints/$cp --dataset_dir=./data/data_CamVidV300/ --logs_path=./logs/$cp/ $@ 2>&1 | tee -a logs/$cp/test.log

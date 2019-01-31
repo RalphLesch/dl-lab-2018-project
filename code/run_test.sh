5d946356706e00ef8c3bb4788c84e1c11f048d22
Ralph Lesch <ralph.lesch@neptun.uni-freiburg.de>
2019-01-31 15:58:32 +0100

    log buffer improvement for run_test.sh

--- code/run_test.sh
+++ code/run_test.sh
@@ -2,4 +2,4 @@
 
 cp=$1
 shift
-python Test_Net.py --model_path=./checkpoints/$cp --dataset_dir=./data/data_CamVidV300/ --logs_path=./logs/$cp/ $@ 2>&1 | tee -a logs/$cp/test.log
+stdbuf -o0 -e0 python3 Test_Net.py --model_path=./checkpoints/$cp --dataset_dir=./data/data_CamVidV300/ --logs_path=./logs/$cp/ $@ 2>&1 | tee -a logs/$cp/test.log

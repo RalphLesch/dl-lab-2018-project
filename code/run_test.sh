3b93276b2c441831a35a3e7c5804521652d4b3b2
Ralph Lesch <ralph.lesch@neptun.uni-freiburg.de>
2019-01-31 20:37:13 +0100

    run_test.sh: make log dir

--- code/run_test.sh
+++ code/run_test.sh
@@ -2,4 +2,5 @@
 
 cp=$1
 shift
+mkdir logs/$cp
 stdbuf -o0 -e0 python3 Test_Net.py --model_path=./checkpoints/$cp --dataset_dir=./data/data_CamVidV300/ --logs_path=./logs/$cp/ $@ 2>&1 | tee -a logs/$cp/test.log

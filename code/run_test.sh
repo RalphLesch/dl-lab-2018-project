15ea6980c5f0fa8e0d922863b4baf850291133b6
Ralph Lesch <ralph.lesch@neptun.uni-freiburg.de>
2019-02-01 18:16:52 +0100

    + Test_Net.py --test_dir option to seperate output dir from model_path, run_test.sh : test output set to .test/

--- code/run_test.sh
+++ code/run_test.sh
@@ -3,4 +3,4 @@
 cp=$1
 shift
 mkdir logs/$cp
-python3 -u Test_Net.py --model_path=./checkpoints/$cp --dataset_dir=./data/data_CamVidV300/ --logs_path=./logs/$cp/ $@ 2>&1 | tee -a logs/$cp/test.log
+python3 -u Test_Net.py --model_path=./checkpoints/$cp --dataset_dir=./data/data_CamVidV300/ --logs_path=./logs/$cp/ --test_dir=./test/$cp/ $@ 2>&1 | tee -a logs/$cp/test.log

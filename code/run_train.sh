c5b47abf7ba4428774c829f6acb3e452fce7db20
Ralph Lesch <ralph.lesch@neptun.uni-freiburg.de>
2019-02-01 16:41:26 +0100

    run_train and test: proper unbuffered output with python3 -u

--- code/run_train.sh
+++ code/run_train.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-#python Test_Net.py --model_path=./checkpoints/ultraslimS_$1 --configuration=$1 >logs/test$1.log 2>&1 &
-python Train_Net.py --checkpoint_dir=./checkpoints/ultraslimS_$1/ --configuration=$1
+# screen -S t1
+# source ../../venv3/bin/activate
+# python3 run_train.sh <subfolder: checkpoint and log> --aug_type=<type>
+
+#python Train_Net.py --checkpoint_dir=./checkpoints/ultraslimS_$1/ --configuration=$1
+#python Train_Net.py --dataset_dir=./data/data_CamVidV300/ --checkpoint_dir=./checkpoints/$2/ --max_steps=$3 --augmentation=$4
+cp=$1
+shift
+
+mkdir logs/$cp
+python3 -u Train_Net.py --dataset_dir=./data/data_CamVidV300/ --checkpoint_dir=./checkpoints/$cp/ --logs_path=./logs/$cp/ --max_steps=21000 $@ 2>&1 | tee -a logs/$cp/train.log
